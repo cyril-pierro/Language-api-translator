243fab1419911bb9bbdacb7591975519818b080e
Cyril Pierro <fiopapa32@gmail.com>
2022-08-12 17:05:17 +0000

    add get_model dependency

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 
 from api.v1 import translate
-from core.config import setting
+from core.config import get_model, setting
+
+get_model()
 
 app = FastAPI(
     docs_url="/",
     version=setting.app_version + setting.release_id,
     title=setting.app_name,
 )
+
 
 app.include_router(translate.router, prefix=setting.endpoint_prefix)
