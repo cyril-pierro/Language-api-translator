92cc68a21ce88982751ca19f4480559e3facc3f2
Cyril Pierro <fiopapa32@gmail.com>
2022-08-12 17:06:03 +0000

    change logic of translate text

--- api/v1/translate.py
+++ api/v1/translate.py
@@ -8,7 +8,7 @@ router = APIRouter()
 
 @router.post("/translate")
 async def translate_text(data: TranslateText):
-    model = await get_model()
+    model = get_model()
     translated_text = model.generate_text(
         f"translate {data.source_language} to \
             {data.destination_language}: {data.input_text}"
